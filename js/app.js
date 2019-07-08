f6f838e527d56d68a2bce089dc84ad02ea0195c4
Jeremy Yandell <jereyand@gmail.com>
2019-07-08 15:49:09 -0700

    add render function to populate values in HTML

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log('sanity check')
+//some variable to work with
+const gameboard = document.querySelector('#gameboard');
+const playerHoles = document.querySelectorAll('.hole');
+const stores = document.querySelectorAll('.store');
+
+gameboard.addEventListener('click', e =>  {
+    console.log(e.target);
+})
+
+const game = {
+    boardArray: [],
+    stores: [],
+    render()  {
+        for (let i = 0; i < playerHoles.length; i++)  {
+            playerHoles[i].innerText = this.boardArray[i];
+        }
+        stores[0].innerText = this.stores[0];
+        stores[1].innerText = this.stores[1];
+    },
+    startGame()  {
+        const len = playerHoles.length;
+        for (let i = 0; i < len; i++)  {
+            this.boardArray.push(4);
+        }
+        this.stores.push(0);
+        this.stores.push(0);
+        this.render();
+    }
+}
+
