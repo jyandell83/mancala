3a05a96d60e09a41084e18885546b773355194b8
Jeremy Yandell <jereyand@gmail.com>
2019-07-08 20:30:52 -0700

    add Player class

--- js/app.js
+++ js/app.js
@@ -3,15 +3,37 @@ const gameboard = document.querySelector('#gameboard');
 const playerHoles = document.querySelectorAll('.hole');
 const stores = document.querySelectorAll('.store');
 
+class Player  {
+    constructor(name, isTurn) {
+        this.name = name;
+        this.boardArray = [4,4,4,4,4,4,0];
+        this.isTurn = isTurn;
+        this.score = 0;
+        this.seedsInHand = 0;
+    }
+    pickUpSeeds ()  {
+        console.log(`${this.name} is picking up seeds`)
+    }
+    dropSeeds ()  {
+        console.log(`${this.name} is dropping seeds`)
+    }
+}
+
 gameboard.addEventListener('click', e =>  {
     if (e.target.id !== 'gameboard' && e.target.id !== 'p1store' && e.target.id !== 'p2store')  {
-    console.log(e.target.id);
+    let string = e.target.id;
+    let newArr = string.split('');
+    newArr.shift();
+    let newString = newArr.join('');
+    let newNumber = parseInt(newString);
+    console.log(newNumber);
     }
 })
 
 const game = {
-    boardArray: [],
-    stores: [],
+    masterBoardArray: [],
+    playerOne: undefined,
+    playerTwo: undefined,
     render()  {
         for (let i = 0; i < playerHoles.length; i++)  {
             playerHoles[i].innerText = this.boardArray[i];
@@ -20,13 +42,11 @@ const game = {
         stores[1].innerText = this.stores[1];
     },
     startGame()  {
-        const len = playerHoles.length;
-        for (let i = 0; i < len; i++)  {
-            this.boardArray.push(i);
-        }
-        this.stores.push(0);
-        this.stores.push(0);
-        this.render();
+        this.playerOne = new Player('Player 1', true);
+        this.playerTwo = new Player('Player 2', false);
+        this.masterBoardArray = this.playerOne.boardArray.concat(this.playerTwo.boardArray);
+        console.log(this.masterBoardArray);
+        // this.render();
     }
 }
 
